c3c9c47d9d0ca056e99328aeed41818a0a41dcf4
Mati219 <63017878+Mati219@users.noreply.github.com>
2021-01-20 20:53:44 +0100

    Added Proptypes

--- src/components/Manage/Movie/MovieManageList.js
+++ src/components/Manage/Movie/MovieManageList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import MovieManageListItem from './MovieManageListItem';
+import PropTypes from 'prop-types';
 
 const MovieManageList = (props) =>(
     <ul className="list-group">
@@ -8,5 +9,11 @@ const MovieManageList = (props) =>(
         ))}
     </ul>
 );
+
+MovieManageList.propTypes = {
+movie: PropTypes.object,
+showEditForm : PropTypes.func,
+showDeleteForm : PropTypes.func
+}
 
 export default MovieManageList;
